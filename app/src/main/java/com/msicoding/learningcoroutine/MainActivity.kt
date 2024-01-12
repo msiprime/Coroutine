9db0e87095223b5f24166d78a88e18296f337898
msiprime <msisakib958@gmail.com>
2024-01-12 16:10:18 +0600

    collect latest done

--- app/src/main/java/com/msicoding/learningcoroutine/MainActivity.kt
+++ app/src/main/java/com/msicoding/learningcoroutine/MainActivity.kt
@@ -3,77 +3,45 @@ package com.msicoding.learningcoroutine
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.runBlocking
 
 
 class MainActivity : ComponentActivity() {
     private val tag = "kotlin_flows"
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         runBlocking {
 
-            flow()
-
-                .flatMapLatest {
-                    flow {
-                        delay(1000)
-                        emit(it * 10)
-                    }
-                }
-                .collect {
-                    Log.d(tag, "Value: $it")
+            flow().onEach {
+                Log.d(tag, "We got: $it")
+            }
+                .collectLatest{
+                    delay(2000)
+                    Log.d(tag, "Result: $it")
                 }
         }
 
     }
 
-    private fun flow(): Flow<Int> = flow {
+    private val list = listOf("Result of Kotlin", "Result of Java", "Result of Swift")
 
-        repeat(5) {
-            emit(it)
-        }
+    private fun flow(): Flow<String> = flow {
+
+        delay(1000)
+        emit(list[0])
+        delay(1000)
+        emit(list[1])
+        delay(1000)
+        emit(list[2])
 
     }
 }
-
-/*
-
-private var timer = 1
-    private fun flow(): Flow<String> = flow {
-
-        emit("This is $timer")
-        delay(1000)
-        timer++
-
-        emit("That is $timer")
-        delay(1000)
-        timer++
-
-        emit("This is $timer")
-        delay(1000)
-        timer++
-
-        emit("That is $timer")
-        delay(1000)
-        timer++
-
-
-    }
-
-* */
-
 
 
 
