a1521bed2bfe043e4f7eb7e215241da9319d5ee8
msiprime <msisakib958@gmail.com>
2024-01-12 12:00:36 +0600

    flow basic done

--- app/src/main/java/com/msicoding/learningcoroutine/MainActivity.kt
+++ app/src/main/java/com/msicoding/learningcoroutine/MainActivity.kt
@@ -1,53 +1,86 @@
 package com.msicoding.learningcoroutine
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.runBlocking
-import kotlin.system.measureTimeMillis
+
 
 class MainActivity : ComponentActivity() {
-    @OptIn(DelicateCoroutinesApi::class)
+    private val tag = "kotlin_flows"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        runBlocking {
 
-        GlobalScope.launch {
-            val x = measureTimeMillis {
+//            getDataFlow().collect { data ->
+//                Log.d(tag, "received: $data")
+//            }
 
-                var file1: String? = null
-                var file2: String? = null
-
-                val job1 = launch {
-                    file1 = getFile()
-                }
-                val job2 = launch {
-                    file2 = getFile2()
-                }
-
-                job1.join()
-                job2.join()
-
-                Log.d("tag_coroutine", "File 1: $file1")
-                Log.d("tag_coroutine", "File 1: $file2")
+            countdownTimer().collect { time ->
+                Log.d(tag, "Counting: $time")
             }
-            Log.d("tag_coroutine", "Total time : $x")
+
         }
     }
 
-    private suspend fun getFile(): String {
-        delay(4000)
-        return "File 1"
+    private fun getDataFlow(): Flow<String> {
+        return flow {
+
+            emit("start  loading...")
+            delay(1000)
+
+            emit("got the data...")
+            delay(1000)
+
+            emit("filtering the data...")
+            delay(1000)
+
+            emit("data is ready...")
+            delay(1000)
+
+            emit("stop loading...")
+
+        }
     }
 
-    private suspend fun getFile2(): String {
-        delay(4000)
-        return "File 2"
+    private fun countdownTimer(): Flow<Int> = flow {
+        var startTimer = 10
+        while (startTimer >= 0) {
+            delay(1000)
+            emit(
+                value = startTimer
+            )
+            startTimer--
+        }
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
