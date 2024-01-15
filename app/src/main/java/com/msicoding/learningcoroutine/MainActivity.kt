f9720ffd2dc08612e7d4fc4209ba21e54dad5206
msiprime <msisakib958@gmail.com>
2024-01-16 01:30:45 +0600

    need to retry this shit

--- app/src/main/java/com/msicoding/learningcoroutine/MainActivity.kt
+++ app/src/main/java/com/msicoding/learningcoroutine/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.msicoding.learningcoroutine
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -9,12 +10,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.msicoding.learningcoroutine.ui.theme.LearningCoroutineTheme
-
 
 class MainActivity : ComponentActivity() {
     private val tag = "kotlin_flows"
@@ -26,6 +28,18 @@ class MainActivity : ComponentActivity() {
             LearningCoroutineTheme {
                 val simpleViewModel1 = viewModel<SimpleViewModel>()
                 val textState = simpleViewModel1.textState.collectAsState()
+
+                val context = LocalContext.current
+                LaunchedEffect(key1 = true) {
+
+                    simpleViewModel1.sharedFlow.collect { show ->
+                        if (show) {
+                            Toast.makeText(context, "Toast", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
+
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
--- app/src/main/java/com/msicoding/learningcoroutine/SimpleViewModel.kt
+++ app/src/main/java/com/msicoding/learningcoroutine/SimpleViewModel.kt
@@ -1,14 +1,35 @@
 package com.msicoding.learningcoroutine
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class SimpleViewModel : ViewModel() {
 
     private val _textState = MutableStateFlow("")  //this is mutable one, can change
-    val textState = _textState.asStateFlow()   // this is immutable , we can't change it but expose it to other
+    val textState =
+        _textState.asStateFlow()   // this is immutable , we can't change it but expose it to other
+
+    private val _sharedFow = MutableSharedFlow<Boolean>()
+    val sharedFlow = _sharedFow.asSharedFlow()
+
+    private val _channel = Channel<Boolean>()
+    val channelFlow = _channel.receiveAsFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(2000)
+            _sharedFow.emit(true)
+        }
+    }
 
     fun changeText(text: String) {
 
